869d4b9614c5fa62fe8dcc990a71b3c8d4e7254e
seahpark247 <famousjedo@gmail.com>
2025-06-02 21:33:47 -0500

    feat: save NavigationStack path using Codable

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,15 +7,57 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
         }
-        .padding()
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Error saving: \(error)")
+        }
+    }
+}
+
+struct DetailView: View {
+    var number: Int
+    
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000)).navigationTitle("Number: \(number)")
+    }
+}
+
+struct ContentView: View {
+    @State private var pathStore = PathStore()
+    
+    var body: some View {
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i)
+                }
+        }
     }
 }
 
